b30f23030b076a70b92e39a2ac3bb36570f97a5c
Sven Kolarić <sven.kolaric@gmail.com>
2018-05-09 17:10:54 +0200

    Initial start

---
+++ ZivotopisIN2/src/main/java/com/DiplomskiRad_SK/ZivotopisIN2/ZivotopisIn2Application.java
@@ -0,0 +1,12 @@
+package com.DiplomskiRad_SK.ZivotopisIN2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZivotopisIn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZivotopisIn2Application.class, args);
+	}
+}
---
+++ ZivotopisIN2/src/test/java/com/DiplomskiRad_SK/ZivotopisIN2/ZivotopisIn2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.DiplomskiRad_SK.ZivotopisIN2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZivotopisIn2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
