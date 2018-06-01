e47686f54fad2aedb0471e2453cb228044bba005
Sven Kolarić <sven.kolaric@gmail.com>
2018-06-01 13:13:38 +0200

    Thinking about moving to jpa - next push will decide

--- ZivotopisIN2/src/main/java/com/DiplomskiRad_SK/ZivotopisIN2/interfaces/DaoCVInterface.java
+++ ZivotopisIN2/src/main/java/com/DiplomskiRad_SK/ZivotopisIN2/interfaces/DaoCVInterface.java
@@ -3,14 +3,46 @@ package com.DiplomskiRad_SK.ZivotopisIN2.interfaces;
 import java.util.List;
 
 import com.DiplomskiRad_SK.ZivotopisIN2.modelDB.CV;
+import com.DiplomskiRad_SK.ZivotopisIN2.modelDB.KontaktniInfo;
+import com.DiplomskiRad_SK.ZivotopisIN2.modelDB.Osoba;
 
 public interface DaoCVInterface {
 
+	// CV
+	
 	void createNewCV(CV cv);
 
 	List<CV> selectAllCV();
 
 	CV selectCVByID(Integer id);
 
-	CV deleteCVByID(Integer id);
+	void deleteCVByID(Integer id);
+	
+	List<CV> selectCVByOsoba(Integer osobaID);
+
+	// Osoba
+
+	void createNewOsoba(Osoba osoba);
+
+	List<Osoba> selectAllOsoba();
+	
+	Osoba selectOsobaByID(Integer id);
+	
+	List<Osoba> selectOsobaByMjesto(Integer mjestoID);
+	
+	void deleteOsobaByID(Integer id);
+	
+	// Kontakt_Info
+	
+	void createNewKontakt_Info(KontaktniInfo kontaktniInfo);
+	
+	List<KontaktniInfo> selectKontakt_InfoByOsoba(Integer osobaID);
+	
+	// Tip_Kontakta
+	
+	// Osoba_Drzavljanstvo
+	
+	// Drzavljanstvo
+	
+	// Drzavljanstvo
 }
